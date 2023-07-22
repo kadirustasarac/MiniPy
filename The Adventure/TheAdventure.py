95530487e82ffbc562fbe29495c7fd23bcc2259d
KadirUstsr <115476449+KadirUstsr@users.noreply.github.com>
2023-07-22 23:42:57 +0300

    Committed

---
+++ The Adventure/TheAdventure.py
@@ -0,0 +1,30 @@
+import random
+import os
+
+def clear():
+    os.system('cls')
+
+clear()
+print("Welcome to the my game ! \n"
+      +"This text-based game about fighting monsters vie halping peasants\n\n"
+      +"Good Luck!".center(20,"*"))
+input("\nPlease press the enter button for playing the game")
+clear()
+
+print("A long long ago even the before human race there was a stone believed that created the universe")
+print("They say if a man can find it he will be a god with unlimited power")
+print("but.... most of the people that trying find that stone never came back")
+("Good luck! you will need that")
+
+def f_TheMonster():
+    print("You saw a giant monster what will you gonna do ? ")
+    print("1-Run away (Monster can spot you by %20 chance and you will die)")
+    print("2-Attack to the Giant (Attakan Titan Referansı)")
+    choice = input()
+
+    if(choice == "1"):
+        pass
+
+while True:
+    fate = random.randrange(1,5)
+
